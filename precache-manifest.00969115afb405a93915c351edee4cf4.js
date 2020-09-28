f513729dffc6f724be5f1c2e3aa19fa400f9ae59
MicLom <mlomzynski94@gmail.com>
2020-09-28 21:21:18 +0200

    Updates

---
+++ precache-manifest.00969115afb405a93915c351edee4cf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bc7c9ac2081d49d1da144621ab88da",
+    "url": "/create-react/index.html"
+  },
+  {
+    "revision": "19bb79a6d9ec78210325",
+    "url": "/create-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ebdfd6df8b05136c597",
+    "url": "/create-react/static/js/2.14350954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react/static/js/2.14350954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19bb79a6d9ec78210325",
+    "url": "/create-react/static/js/main.eed51cb2.chunk.js"
+  },
+  {
+    "revision": "d2b644ea14187be45f51",
+    "url": "/create-react/static/js/runtime-main.2bc5e9bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
